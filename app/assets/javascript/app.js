088e438db60079354bec95bca03f8664d440588c
Ville Ahti <villeahti@hotmail.com>
2014-10-23 08:55:21 +0300

    day navigation

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,26 +1,53 @@
-var weekdays = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai'];
-var today = new Date();
-var selectedDay = today;
-var selectedWeekDay = getCurrentWeekDay(selectedDay);
+var WEEK_DAY_NAMES = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai'];
+var today, selectedDay, selectedWeekDay, selectedWeekDayName;
+var foodView, navigationView, wall;
 
-function getCurrentWeekDay(date) {
-  var dayStartingSunday = date.getDay();
-  return dayStartingSunday < 6 ? dayStartingSunday - 1 : 0;
-};
+function initViewModels() {
+  today = new Date();
+  selectedDay = new Date(today.getTime());
+  selectedWeekDay = getWeekDayNumber(selectedDay);
+  selectedWeekDayName = WEEK_DAY_NAMES[selectedWeekDay];
 
-var ractive, wall;
-initRactive();
+  navigationView = new Ractive({
+      el: 'day-navigation',
+      template: '#navigation-template',
+      data: {
+        today : today,
+        selectedDay: selectedDay,
+        selectedWeekDay: selectedWeekDay,
+        selectedWeekDayName: selectedWeekDayName,
+        formatDate: formatDate    
+      }
+    });
 
+  navigationView.on({
+    navigatePreviousDay: function() {
+      var previousDay = navigationView.get('selectedDay');
+      previousDay.setDate(previousDay.getDate()-1);
 
-$(function() {
+      navigationView.set({
+        selectedDay: previousDay,
+        selectedWeekDay: getWeekDayNumber(previousDay),
+        selectedWeekDayName: WEEK_DAY_NAMES[getWeekDayNumber(previousDay)]
+      });
 
-    getFoods(selectedWeekDay);
+      showFoodsFor(getWeekDayNumber(previousDay));
+    },
+    navigateNextDay: function() {
+      var nextDay = navigationView.get('selectedDay');
+      nextDay.setDate(nextDay.getDate()+1);
 
-    initWall();
-});
+      navigationView.set({
+        selectedDay: nextDay,
+        selectedWeekDay: getWeekDayNumber(nextDay),
+        selectedWeekDayName: WEEK_DAY_NAMES[getWeekDayNumber(nextDay)]
+      });
 
-function initRactive() {
-  ractive = new Ractive({
+      showFoodsFor(getWeekDayNumber(nextDay));
+    }
+  });
+
+  foodView = new Ractive({
       el: 'food-container',
       template: '#page-template',
       data: {
@@ -30,6 +57,31 @@ function initRactive() {
       }
     });
 }
+
+function formatDate(date) {
+  var day = date.getDate();
+  var month = date.getMonth();
+  month++;
+  var year = date.getFullYear();
+  return day + "." + month + "." + year;
+}
+
+function getWeekDayNumber(date) {
+  var dayStartingSunday = date.getDay();
+  return dayStartingSunday == 0 ? 6 : dayStartingSunday - 1;
+};
+
+function formatPrices(prices) {
+  return prices.join(' / ');
+}
+
+$(function() {
+  initViewModels();
+
+  showFoodsFor(selectedWeekDay);
+
+  initWall();
+});
 
 function initWall() {
   wall = new freewall("#food-container");
@@ -45,19 +97,16 @@ function initWall() {
     wall.fitWidth();
 }
 
-function formatPrices(prices) {
-  return prices.join(' / ');
-}
 
-function getFoods(weekday) {
-  ractive.set('loadingFoods', true);
+function showFoodsFor(weekday) {
+  foodView.set('loadingFoods', true);
   
   $.ajax({
         url: "2014_w18_unica.json"
     
     }).then(function(data) {   
-       ractive.set('foodsByRestaurant', data.foodsByDay[weekday].foodsByRestaurant);
-       ractive.set('loadingFoods', false);
+       foodView.set('foodsByRestaurant', data.foodsByDay[weekday].foodsByRestaurant);
+       foodView.set('loadingFoods', false);
        wall.refresh();
     });
 }
